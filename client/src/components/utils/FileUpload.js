5599060a1829ec8fd70ad3ed127b9a387928f17b
OH-JOO-YEONG <brb1111@naver.com>
2021-04-11 19:31:19 +0900

    Feat: 업로드 페이지 기능 구현2 - 파일 업로드 컴포넌트 구현 완료(Dropzone 모듈로 파일 업로드, 이미지 미리보기, 이미지 삭제) - Upload 이미지 서버 저장 - Product DB 설계, 서버 라우터 연결 - Product 데이터 DB에 저장 및 랜딩페이지 푸시
    Resolves #2

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropzone from "react-dropzone";
 import { Icon } from "antd";
+import axios from "axios";
 
-const FileUpload = () => {
+const FileUpload = (props) => {
+    const [Images, setImages] = useState([]);
+
+    const dropHandler = (files) => {
+        let formData = new FormData();
+        const config = {
+            header: { "content-type": "multipart/form-data" },
+        };
+        formData.append("file", files[0]);
+        axios.post("/api/product/image", formData, config).then((response) => {
+            if (response.data.success) {
+                setImages([...Images, response.data.filePath]);
+                props.refreshFunction([...Images, response.data.filePath]);
+            } else {
+                alert("파일을 저장하는 데 실패했습니다.");
+            }
+        });
+    };
+    const deleteHandler = (image) => {
+        const currentIndex = Images.indexOf(image);
+
+        let newImages = [...Images];
+        newImages.splice(currentIndex, 1);
+        setImages(newImages);
+        props.refreshFunction(newImages);
+    };
+
     return (
         <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+            <Dropzone onDrop={dropHandler}>
                 {({ getRootProps, getInputProps }) => (
                     <section>
                         <div
@@ -25,6 +52,16 @@ const FileUpload = () => {
                     </section>
                 )}
             </Dropzone>
+            <div style={{ display: "flex", width: "300px", height: "257px", overflowX: "scroll" }}>
+                {Images.map((image, index) => (
+                    <div onClick={() => deleteHandler(image)} key={index}>
+                        <img
+                            style={{ maxWidth: "300px", width: "300px", height: "240px" }}
+                            src={`http://localhost:5000/${image}`}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Typography, Button, Form, Input } from "antd";
 import FileUpload from "../../utils/FileUpload";
+import Axios from "axios";
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -15,12 +16,12 @@ const Continents = [
     { key: 7, value: "남극" },
 ];
 
-const UploadProductPage = () => {
+const UploadProductPage = (props) => {
     const [Title, setTitle] = useState("");
     const [Description, setDescription] = useState("");
     const [Price, setPrice] = useState(0);
     const [Continent, setContinent] = useState(1);
-    const [Image, setImage] = useState([]);
+    const [Images, setImages] = useState([]);
 
     const titleChangeHandler = (e) => {
         setTitle(e.currentTarget.value);
@@ -38,13 +39,43 @@ const UploadProductPage = () => {
         setContinent(e.currentTarget.value);
     };
 
+    const updateImages = (newImages) => {
+        setImages(newImages);
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if (!Title || !Description || !Price || !Continent || !Images) {
+            return alert("모든 값을 넣어주셔야 합니다.");
+        }
+
+        // 서버에 채운 값들을 request로 보낸다.
+        const body = {
+            // 로그인 된 사람의 ID
+            writer: props.user.userData._id,
+            title: Title,
+            description: Description,
+            price: Price,
+            images: Images,
+            continents: Continent,
+        };
+        Axios.post("/api/product", body).then((response) => {
+            if (response.data.success) {
+                alert("상품 업로드에 성공했습니다.");
+                props.history.push("/");
+            } else {
+                alert("상품 업로드에 실패했습니다.");
+            }
+        });
+    };
+
     return (
         <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
             <div style={{ textAlign: "center", marginBottom: "2rem" }}>
                 <h2 level={2}>여행 상품 업로드</h2>
             </div>
-            <Form>
-                <FileUpload />
+            <Form onSubmit={submitHandler}>
+                <FileUpload refreshFunction={updateImages} />
                 <br />
                 <br />
                 <label>이름</label>
@@ -68,7 +99,7 @@ const UploadProductPage = () => {
                 </select>
                 <br />
                 <br />
-                <Button>확인</Button>
+                <Button htmlType="submit">확인</Button>
             </Form>
         </div>
     );
